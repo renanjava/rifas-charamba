d327cd8027323c481c35aa9385f90b3ff2fa3195
Renan <renancvr777@gmail.com>
2025-11-08 02:15:02 -0300

    feat: update real product ant payment link

--- src/constants/global.constants.ts
+++ src/constants/global.constants.ts
@@ -1,5 +1,5 @@
 export const PAYMENT_URL =
-  "https://loja.infinitepay.io/renan-g-l/akc2405-bilhete-iphone-xr";
+  "https://loja.infinitepay.io/renan-g-l/hpt4743-bilhete-premiado---corte-de-cabelo";
 
 export const PARTICIPANTS_URL =
   "https://docs.google.com/spreadsheets/d/1Cdv7BDkcCWYcTh35HD-C6xrlhwlpZrd0pVXAX1ecCUk/edit?usp=sharing";
